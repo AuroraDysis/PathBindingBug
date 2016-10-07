66ca032e6b0b1b2a141568630ed9411b8abcf716
AuroraDysis <auroradysis@gmail.com>
2016-10-07 22:18:47 +0800

    develop

--- App/MainPage.xaml.cs
+++ App/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -10,6 +11,7 @@ using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Markup;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
@@ -25,6 +27,30 @@ namespace App
         public MainPage()
         {
             this.InitializeComponent();
+            var circle = Parse("M 0,0 A 180,180 180 1 1 1,1 Z");
+            this.Items = new List<TestItem>()
+            {
+                new TestItem() {Data = circle },
+                new TestItem() {Data = circle },
+                new TestItem() {Data = circle },
+                new TestItem() {Data = circle },
+            };
         }
+
+        public List<TestItem> Items { get; set; }
+
+        public static Geometry Parse(string data)
+        {
+            var sb = new StringBuilder();
+            sb.Append("<Geometry xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'>");
+            sb.Append(data);
+            sb.Append("</Geometry>");
+            return (Geometry)XamlReader.Load(sb.ToString());
+        }
+    }
+
+    public class TestItem
+    {
+        public Geometry Data { get; set; }
     }
 }
